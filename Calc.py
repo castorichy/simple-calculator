b34e4010ac1c9f30132b2fc664fc5591fe734130
castorichy <castorichy@castorichy.castorichy>
2023-02-28 17:38:28 +0300

    removing decimals when its 0

---
+++ Calc.py
@@ -0,0 +1,35 @@
+from . import 
+"""A simple calculator that doese some simple calculations"""
+"""
+\tJust a simple Calculator
+Opertors to be used:
+        add = +
+        subtract = -
+        multiplication = *
+        Devision = /
+        Reminder = %
+    Example:
+    2 + 2 - 3 * 4 % 3 / 3
+Also remember to seperate number and operator with one space in between"""
+def _validate(self):
+    strg = input("""\
+\tJust a simple Calculator
+Opertors to be used:
+    add = +
+    subtract = -
+    multiplication = *
+    Devision = /
+    Reminder = % not implimented
+Example:
+2 + 2 - 3 * 4 / 3
+Also remember to seperate number and operator with one space in betwee
+Enter math operatoion to oparate
+>>> """)
+    # Creating object
+        
+
+
+
+cal = Calculator()
+res = cal._validate()
+print(res)
---
+++ test.py
@@ -0,0 +1,69 @@
+import os
+from posix import system
+
+class Calculator:
+    """Simple Calculator that calculates mixture of operations in one line
+    Examole: 2 + 4 * 3 / 2 = 8"""
+    __op = [ "/", "*", "+", "-" ]
+    __test = []
+    __res = 0.0
+    def __init__(self, string: str) -> None:
+        """Initializes the Atributes"""
+        self.__op = Calculator.__op
+        self.__test = Calculator.__test
+        self.__res = Calculator.__res
+        self.__test = string.split(" ")
+
+    def calculate(self) -> float:
+        """Doese All the calculations necessary and return the results"""
+        for e in self.__op:
+        # loops through op (operators)
+            for j, f in enumerate(self.__test):
+            # loops through test (values to work on -- test)
+                if e == f:
+                    match f:
+                        case "/":
+                            self.__res = float(self.__test[j - 1]) / float(self.__test[j + 1])
+                            self.__removes(j)
+                        case "*":
+                            self.__res = float(self.__test[j - 1]) * float(self.__test[j + 1])
+                            self.__removes(j)
+                        case "+":
+                            self.__res = float(self.__test[j - 1]) + float(self.__test[j + 1])
+                            self.__removes(j)
+                        case "-":
+                            self.__res = float(self.__test[j - 1]) - float(self.__test[j + 1])
+                            self.__removes(j)
+        return self.__res
+
+
+    def __removes(self, j):
+        """Deletes value used in test list and inserts final results"""
+        self.__test.pop(j + 1)
+        self.__test.pop(j)
+        self.__test.pop(j - 1)
+        self.__test.insert(j - 1, str(self.__res))
+
+
+
+def createObject():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    strg = input("""\
+\tJust a simple Calculator
+Opertors to be used:
+    add = +
+    subtract = -
+    multiplication = *
+    Devision = /
+    Reminder = % not implimented
+Example:
+1 + 2 - 3 * 4 / 3
+Also remember to seperate number and operator with one space in betwee
+\tEnter math operatoion
+>>> """)
+    # Creating object
+    calc = Calculator(strg)
+    result = calc.calculate()
+    print("\nResult =", result)
+
+createObject()
